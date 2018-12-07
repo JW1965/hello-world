2b329ffdc9352e60d9ef4e7b94fbfd9efa93d912
jensw <jensw@Saopaulo.fritz.box>
2018-12-07 07:37:17 +0100

    using Push branch master

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -3,6 +3,6 @@ public class helloworld {
 	
 
 	public static void main(String[] args){
-		System.out.println ("hello world\n");
+		System.out.println ("hello world with team Push brach master 01\n");
 	}
 }

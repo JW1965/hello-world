54fea28053c54d55d701deca3630a6703a57481e
jensw <jensw@Saopaulo.fritz.box>
2018-12-25 18:12:04 +0100

    new try

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -3,6 +3,6 @@ public class helloworld {
 	
 
 	public static void main(String[] args){
-		System.out.println ("hello world with team Push brach master 01\n");
+		System.out.println ("hello world with team Push branch master edit 07\n");
 	}
 }
